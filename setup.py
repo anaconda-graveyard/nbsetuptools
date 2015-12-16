6d852af2a41501f807908742dffb7a241b145d20
Marcos Vanetta <marcosvanetta@gmail.com>
2015-12-16 11:32:35 -0600

    Release 0.1.3

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 
 setup(
     name="nbsetuptools",
-    version='0.1.0',
+    version='0.1.2',
     url="https://github.com/anaconda-server/anaconda-notebook",
     author="Continuum Analytics",
     author_email="info@continuum.io",
     description="Help you install your nbextensions",
-    long_description=open('README.md').read(),
     packages=['nbsetuptools'],
+    license='BSD'
 )
