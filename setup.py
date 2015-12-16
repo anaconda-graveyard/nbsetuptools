6acadcba90d0605af0077d65ea527931be4d1ab7
Marcos Vanetta <marcosvanetta@gmail.com>
2015-12-16 12:32:35 -0600

    Dependencies and 0.1.4

--- setup.py
+++ setup.py
@@ -3,11 +3,15 @@ from distutils.core import setup
 
 setup(
     name="nbsetuptools",
-    version='0.1.3',
+    version='0.1.4',
     url="https://github.com/anaconda-server/anaconda-notebook",
     author="Continuum Analytics",
     author_email="info@continuum.io",
     description="Help you install your nbextensions",
     packages=['nbsetuptools'],
-    license='BSD'
+    license='BSD',
+    install_requires=[
+        'funcsigs',
+        'jupyter'
+    ]
 )
