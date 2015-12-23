da1e20574c8c470ae364937aaf526b634ab880ea
Michael Marchetti <mmarchetti@continuum.io>
2015-12-23 09:45:21 -0500

    Fix URL in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name="nbsetuptools",
     version='0.1.4',
-    url="https://github.com/anaconda-server/anaconda-notebook",
+    url="https://github.com/anaconda-server/nbsetuptools",
     author="Continuum Analytics",
     author_email="info@continuum.io",
     description="Help you install your nbextensions",
