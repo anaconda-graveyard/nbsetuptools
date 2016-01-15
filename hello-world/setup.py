9c08ae372fad2ec5a6984dc4530072168a33e914
damianavila <damianavila@gmail.com>
2016-01-15 11:20:00 -0300

    Replace anaconda-notebook with anaconda-nb-extensions. Also make the .binstar file in sync with others.

--- hello-world/setup.py
+++ hello-world/setup.py
@@ -5,7 +5,7 @@ from hello_world import _version
 setuptools.setup(
     name="hello_world",
     version=_version.__version__,
-    url="https://github.com/anaconda-server/anaconda-notebook",
+    url="https://github.com/anaconda-server/anaconda-nb-extensions",
     author="Continuum Analytics",
     author_email="info@continuum.io",
     description="Example of a server extension for Jupyter notebook",
